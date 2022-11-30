01b7e21e8b45263347741a698f60f024e20d454e
sulthan441 <mohammedsulthan441@gmail.com>
2022-12-01 03:39:48 +0530

    product

---
+++ product/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Products } from './components/product';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Products/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ product/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ChakraProvider } from '@chakra-ui/react'
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <ChakraProvider>
+        <App/>
+  </ChakraProvider>
+);
+
+reportWebVitals();
